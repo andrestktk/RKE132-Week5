fde46c7fa1742b9e890cd3c71a036c2c9296ffd8
Andrestktk <andres.altmets@tktk.ee>
2023-05-03 23:34:56 +0300

    add Week5LogicalOperators code

---
+++ OrOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Math >= 90 or Chemistry >=90;
+
+//math && chemistry || math && Biology || chemisty && biology
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enrer you Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enrer you Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (chemistry >= 99 && biology >= 90) || (math >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved!");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//and - && operaator
+
+// true && true --> true
+// true && false --> false
+// false && true --> false
+// false && false --> false
+
+// or - || (pipes)
+// true || true --> true
+// true || false --> true
+// false || true --> true
+// false || false --> false
+
